f507612f5df0a9aa224ba8cabc3fb7b16b24481f
Monishapleo <monishapleo@gmail.com>
2025-11-12 22:18:29 +0530

    Initial commit

---
+++ orgconnect/src/main/java/com/orgconnect/OrgconnectApplication.java
@@ -0,0 +1,13 @@
+package com.orgconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrgconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrgconnectApplication.class, args);
+	}
+
+}
